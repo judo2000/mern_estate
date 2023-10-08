2279d157a6f10252d33cb6cd54dff9392c844c29
Stephen Moore <judo2000@comcast.net>
2023-10-08 11:57:43 -0600

    Connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Conntected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
