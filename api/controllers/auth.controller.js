8ffb4dd3963fb600c2433e0def863b5a968c9c86
Stephen Moore <judo2000@comcast.net>
2023-10-08 15:47:13 -0600

    Complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,24 +20,21 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(401), "Invalid Credentials");
+    if (!validUser) return next(errorHandler(404, "Invalid credentials!"));
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401), "Invalid Credentials");
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials!"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: pass, ...userInfo } = validUser._doc;
+
+    const { password: pass, ...rest } = validUser._doc;
+
     res
-      .cookie("access_token", token, {
-        httpOnly: true,
-        //maxAge: 24 * 60 * 60 * 1000,
-        //expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
-      })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(userInfo);
+      .json(rest);
   } catch (error) {
     next(error);
   }
