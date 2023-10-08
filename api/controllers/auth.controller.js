82ee352edffe90e0c46e4cb185959b419ed3e79a
Stephen Moore <judo2000@comcast.net>
2023-10-08 15:23:40 -0600

    Create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +13,31 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ messate: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(401), "Invalid Credentials");
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401), "Invalid Credentials");
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        //maxAge: 24 * 60 * 60 * 1000,
+        //expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
